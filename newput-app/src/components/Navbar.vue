9cf2e55a76112153592216982fc54b6053b326de
kishore-6398 <81747570+kishore-6398@users.noreply.github.com>
2021-05-15 00:58:35 -0400

    added icons to navbar

--- newput-app/src/components/Navbar.vue
+++ newput-app/src/components/Navbar.vue
@@ -3,22 +3,66 @@
       
       <nav id="mainNav">
         <span class="menu material-icons" @click="navToggleMethod">menu</span>
-        <router-link to="/" class="titleLogo" @click="navToggleFalse">NEWPUT</router-link> 
+        <router-link to="/" class="titleLogo" @click="navToggleFalse">
+            <img src="../assets/newputLogo.jpg" class="newputIcon" alt="newput icon">
+            <span class="newput">NEWPUT</span>
+        </router-link> 
       </nav>
 
       <nav id="sidenav" class="sidenav" :class="{ sidenavActive: navToggle }">
         <div class="logo">
-            <router-link to="/" >NEWPUT</router-link>
+            <router-link to="/" >
+                <img src="../assets/newputLogo.jpg" class="newputIcon" alt="newput icon">
+                <span class="newput">NEWPUT</span>
+            </router-link>
         </div>
+
+        <div class="userprofile">
+            <div class="username">
+                <img src="../assets/avatar.png" class="userIcon" alt="user icon">
+                <div class="nameText text-wrap">{{ name }}</div>
+            </div>
+            <div class="usermail">
+                <span class="material-icons md-mail">mail</span>
+                <div class="emailText text-wrap">{{ email }}</div>
+            </div>
+        </div>
+
+        <div class="hrnav"></div>
+        
         <ul @click="navToggleMethod">
-            <router-link class="routeLink" to="/todo" >Todo List</router-link>
-            <router-link class="routeLink" to="/pvault" >Password Vault</router-link>
-            <router-link class="routeLink" to="/url" >URL Manager</router-link>
-            <router-link class="routeLink" to="/notes" >Notes Editor</router-link>
-            <router-link class="routeLink" to="/expense/form" >Expense Tracker</router-link>
-            <router-link class="routeLink" to="/weather" >World Weather</router-link>
-            <li @click="logout" class="routeLink">LogOut</li>
+            <router-link class="routeLink" to="/todo" >
+                <span class="material-icons md-task">task</span>
+                <span class="routeText">Todo List</span>
+            </router-link>
+            <router-link class="routeLink" to="/pvault" >
+                <span class="material-icons md-lock">lock</span>
+                <span class="routeText">Password Vault</span>
+            </router-link>
+            <router-link class="routeLink" to="/url" >
+                <span class="material-icons md-publicurl">public</span>
+                <span class="routeText">URL Manager</span>
+            </router-link>
+            <router-link class="routeLink" to="/notes" >
+                <span class="material-icons md-description">description</span>
+                <span class="routeText">Notes Editor</span>
+            </router-link>
+            <router-link class="routeLink" to="/expense/form" >
+                <span class="material-icons md-paid">paid</span>
+                <span class="routeText">Expense Tracker</span>
+            </router-link>
+            <router-link class="routeLink" to="/weather" >
+                <span class="material-icons md-cloud">cloud</span>
+                <span class="routeText">World Weather</span>
+            </router-link>
         </ul>
+
+        <div @click="logout" class="logout">
+            <span class="material-icons md-logout">logout</span>
+            <span class="routeText logoutText">LogOut</span>
+        </div>
+     
+        
         <div class="overlay" @click="navToggleMethod"></div>
       </nav>
 
@@ -26,13 +70,16 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore }from 'vuex';
 
 export default {
     setup(){
         var navToggle = ref(false);
         var store = useStore();
+
+        var name = computed(() => store.getters.getUsername);
+        var email = computed(() => store.getters.getuserEmail);
 
         function navToggleMethod(){
             navToggle.value = !navToggle.value;
@@ -52,7 +99,9 @@ export default {
             navToggle,
             navToggleMethod,
             navToggleFalse,
-            logout
+            logout,
+            name,
+            email
         }
     } 
 }
@@ -63,16 +112,18 @@ export default {
     #mainNav{
         display: none;
     }
+    .newput{
+        margin-left: 5px;
+    }
     .logo{
         text-align: center;
-        margin: 10px 0 0 0;
+        margin: 15px 0 30px 0;
     }
     .logo a{
         text-decoration: none;
         color: white;
-        font-size:x-large;
-        font-weight: bolder;
-        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size:31px;
+        font-family:'Kalam', cursive;   
     }
     .logo a:hover{
         color: white;
@@ -80,35 +131,114 @@ export default {
     #sidenav ul{
         list-style: none;
         padding: 0;
-        margin: 20px 0 0 0;
+        margin: 0;
         display: flex;
         flex-direction: column;
     }
     
     #sidenav ul .routeLink{
-        padding: 20px 0 20px 10px;
+        padding: 15px 0 15px 20px;
         text-decoration: none;
         color: white;
-        font-size: 17px;
+        font-size: 16px;
         transition: 0.3s;
+        display: flex;
     }
     #sidenav ul .routeLink:hover{
         background-color: rgb(157, 83, 226);
         cursor: pointer;
     }
+    #sidenav .logout{
+        padding: 15px 0 15px 20px;
+        text-decoration: none;
+        color: white;
+        font-size: 16px;
+        transition: 0.3s;
+        display: flex;
+    }
+    #sidenav .logout:hover{
+        background-color: rgb(157, 83, 226);
+        cursor: pointer;
+    }
+    .routeText{
+        padding-left: 20px;
+    }
     .overlay{
         visibility: hidden;
+    }
+    .hrnav{
+        height: 0.5px;
+        width: auto;
+        background-color: white;
+        margin: 20px 0 0 0;
+    }
+    .userprofile{
+        margin: 15px 0 0 20px;
+        color: white;
+    }
+    .userIcon{
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+    }
+    .newputIcon{
+        border-radius: 50%;
+        width: 45px;
+        height: 45px;
+    }
+    .username, .usermail{
+        display: flex;
+        align-items: center;
+    }
+    .usermail{
+        margin-top: 10px;
+    }
+    .nameText{
+        margin-left: 14px;
+        word-wrap: break-word;
+        width: 170px;
+    }
+    .emailText{
+        margin-left: 20px;
+        word-wrap: break-word;
+        width: 170px;
+    }
+
+
+    .md-task{
+        color: rgb(238, 255, 0);
+    }
+    .md-lock{
+        color: rgb(9, 255, 0);
+    }
+    .md-publicurl{
+        color: rgb(0, 255, 234);
+    }
+    .md-description{
+        color: rgb(255, 123, 0);
+    }
+    .md-paid{
+        color: rgb(0, 255, 170);
+    }
+    .md-cloud{
+        color: rgb(0, 195, 255);
+    }
+    .md-logout{
+        color: rgb(255, 255, 255);
+    }
+    .md-mail{
+        color:rgb(226, 167, 253);
     }
 
     @media (max-width: 850px){
         .sidenav{
-            position: fixed ;
-            top:58px;
+            position: fixed;
+            top: 58px;
             left: -100%;
             background-color: gray;
             height: calc(100vh - 58px);
             transition: 0.5s;
-            width: 260px;
+            width: 270px;
             overflow-y:auto;
             scroll-behavior:smooth;
         }
@@ -116,11 +246,14 @@ export default {
             left: 0;
             z-index: 3 !important;
         }
+        #sidenav .logout{
+            margin-bottom: 100px;
+        }
         .sidenavActive .overlay{
             visibility: visible;
             position: fixed !important;
             top:58px;
-            left: 260px;
+            left: 270px;
             right: 0;
             bottom: 0;
             z-index: 3 !important;
@@ -133,15 +266,14 @@ export default {
             width: 100%;
             background-color: blueviolet;
             margin-top: 0;
-            display: inline-flex;
+            display: flex;
         }
         #mainNav .titleLogo{
             margin: auto;
             text-decoration: none;
             color: white;
-            font-size:x-large;
-            font-weight: bolder;
-            font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-size:30px;
+            font-family:'Kalam', cursive; 
         }
         
         #mainNav .menu{
